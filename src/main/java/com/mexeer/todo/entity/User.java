e79641b3368dd1a7e91fafff6c32a62d4761c059
thilinam <thilinamanawadu@gmail.com>
2024-10-18 13:54:13 +0530

    # user entity validations

--- src/main/java/com/mexeer/todo/entity/User.java
+++ src/main/java/com/mexeer/todo/entity/User.java
@@ -1,9 +1,12 @@
 package com.mexeer.todo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
 
 import java.util.List;
 
@@ -14,11 +17,16 @@ public class User extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "field should not be null") @NotEmpty(message = "field should not be blank")
     private String firstName;
+    @NotNull(message = "field should not be null") @NotEmpty(message = "field should not be blank")
     private String lastName;
     @Enumerated(EnumType.STRING)
     private Gender gender;
+    @NotNull(message = "field should not be null") @NotEmpty(message = "field should not be blank")
     private String email;
+    @NotNull(message = "field should not be null") @NotEmpty(message = "field should not be blank")
     private String password;
 
     @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
