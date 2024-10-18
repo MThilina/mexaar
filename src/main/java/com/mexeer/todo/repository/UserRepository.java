815608ffe8bb9be6190a5bfb129c520474a3caa6
thilinam <thilinamanawadu@gmail.com>
2024-10-18 13:54:43 +0530

    # repository custom queryies

--- src/main/java/com/mexeer/todo/repository/UserRepository.java
+++ src/main/java/com/mexeer/todo/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.mexeer.todo.repository;
 
 import com.mexeer.todo.entity.User;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public interface UserRepository extends GenericRepository<User,Long> {
+    boolean existsByEmail(@NotNull(message = "field should not be null") @NotEmpty(message = "field should not be blank") String email);
 }
