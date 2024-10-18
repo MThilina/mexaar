61785e31d132d2c63d0576c88cb3fd57546bd487
thilinam <thilinamanawadu@gmail.com>
2024-10-18 18:22:52 +0530

    # interface contract to task

---
+++ src/main/java/com/mexeer/todo/service/TaskService.java
@@ -0,0 +1,21 @@
+package com.mexeer.todo.service;
+
+import com.mexeer.todo.entity.Task;
+import com.mexeer.todo.entity.TaskStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskService extends GenericService<Task,Long>{
+    Task createTask(Long todoId, Task task);
+
+    List<Task> getTasksByToDoListId(Long todoId);
+
+    Optional<Task> getTaskById(Long taskId);
+
+    Task updateTask(Long taskId, Task task);
+
+    Task updateTaskStatus(Long taskId, TaskStatus status);
+
+    void deleteTask(Long taskId);
+}
