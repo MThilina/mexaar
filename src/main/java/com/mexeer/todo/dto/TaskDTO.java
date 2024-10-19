81fee34f3065f0be472939994d5a8ac316216de8
thilinam <thilinamanawadu@gmail.com>
2024-10-19 10:36:03 +0530

    # refine json code and form

--- src/main/java/com/mexeer/todo/dto/TaskDTO.java
+++ src/main/java/com/mexeer/todo/dto/TaskDTO.java
@@ -1,10 +1,12 @@
 package com.mexeer.todo.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TaskDTO {
     private Long id;
     private String name;
--- src/main/java/com/mexeer/todo/dto/ToDoListDTO.java
+++ src/main/java/com/mexeer/todo/dto/ToDoListDTO.java
@@ -1,9 +1,11 @@
 package com.mexeer.todo.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.mexeer.todo.entity.User;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ToDoListDTO {
     private Long id;
     private String name;
